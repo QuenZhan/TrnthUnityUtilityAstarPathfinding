5739d67e8c436de814a3c1e69689448b78e18a7c
JimbowMBPR <adeathjohn@gmail.com>
2015-02-05 22:33:09 +0800

    sdf

---
+++ TrnthAStarRescan.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthAStarRescan : MonoBehaviour {
+	public float delayBetween=5;
+	void execute(){
+		if(!AstarPath.active)return;
+		AstarPath.active.Scan();
+		Invoke("execute",delayBetween);
+	}
+	void Start(){
+		execute();
+	}
+}
