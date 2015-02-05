606bdd06bf9004cbfe80f3e2510c31616b28fff5
JimbowMBPR <adeathjohn@gmail.com>
2015-02-05 22:36:12 +0800

    action

---
+++ TrnthHVSActionAstarScan.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrnthHVSActionAstarScan : TrnthHVSAction {
+	protected override void _execute(){
+		base._execute();
+		AstarPath.active.Scan();
+	}
+}
